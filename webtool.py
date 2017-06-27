4fc47609ffebe906a3eeb1be451f66fa787dbb87
baileywickham <baileywickham@gmail.com>
2017-06-27 13:38:01 -0700

    completly fixed and rewrote webtool.py. login page now works.

--- webtool.py
+++ webtool.py
@@ -9,13 +9,41 @@ from flask_login import current_user
 app = Flask(__name__)
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
+users = {'admin': 'password'}
 
-
-@app.route('/', methods=['GET', 'POST'])
-def my_form():
+@app.route('/')
+def splash():
         return render_template('login.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+        if request.method == 'POST' and 'inputUsername' in request.form:
+                username = request.form['inputUsername']
+                pw = request.form['inputPassword']
+        user_id = 1234
+        User = UserClass(username, user_id, active=True)
+        if username == 'admin' and pw == 'password':
+                login_user()
+                return render_template('index.html')
+        else:
+                print ('incorrect password')
+        return render_template('login.html')
+
+def login_user():
+        pass
+def index():
+        return render_template('index.html')
+@login_manager.user_loader
+def user_loader(userid):
+        pass
 
 
+class UserClass(flask_login.UserMixin):
+        def __init__(self, name, id, active=True):
+                self.name = name
+                self.id = id
+                self.active = active
+def is_active(self):
+        return self.active
 if __name__ == '__main__':
         app.run(debug=True)
