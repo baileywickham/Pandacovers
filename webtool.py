2dccff8b3984e576f7aeeab7e09ab142437e8485
baileywickham <baileywickham@gmail.com>
2017-06-30 02:02:00 -0500

    attemped to fix webtool.py, Failed.

--- webtool.py
+++ webtool.py
@@ -16,17 +16,19 @@ db = MySQLdb.connect(host="localhost",
 		     user="root",
 		     passwd="alexiscool",
 		     db="panda-login")
-cur = db.cursor() # Cursor is needed for executing queries
+#CURSORS MUST BE INSIDE METHODS OR ELSE IT CRASHES, NO GLOBAL CURSORS. cur = db.cursor()
 
 def user_exists(username):
+	cur = db.cursor()
 	if cur.execute("SELECT * FROM Users WHERE username = '" + username + "'") != 0:
 		return True
 	return False
 	# Non-zero value indicates that the user exists
 
 def get_password(username):
-	return cur.execute("SELECT password FROM Users WHERE username = '" + username + "'") #this is the line that is causing the problem.
-
+	cur = db.cursor()
+	localpw = cur.execute("SELECT password FROM Users WHERE username = '" + username + "'") #this is the line that is causing the problem.	
+	return localpw
 #return loginpage on first login
 @app.route('/')
 def splash():
@@ -36,19 +38,24 @@ def splash():
 def login():
         #fake userid because database doesnt exist yet. replace username == admin with a database check,
         #including checking passwords
-        if request.method == 'POST' and 'inputUsername' in request.form:
+        pw = 'hello world'
+	if request.method == 'POST' and 'inputUsername' in request.form:
                 username = request.form['inputUsername']
-                pw = request.form['inputPassword']
+                pw = request.form.['inputPassword']
 	if not user_exists(username):
-		flash('incorrect username or password') # Placeholder file, we need to sort out an error page
+		flash('incorrect username') # Placeholder file, we need to sort out an error page
 	password = get_password(username)
         user_id = 1234
-
+	target = open('tmp', 'w')
+	target1 = open('tmp1', 'w') #what the actual fuck, reqest.form input password return an int. Why does it return an int??? it returns a one or a 0.
+	target.write(str(password))
+	target1.write(username) #check tmp for local pw, check tmp1 for password from server. password from server is always correct but pw from from returns an int.
         User = UserClass(username, user_id, active=True)
-        if pw == password: # pw and password do not appear to be the same. Needs to be fixed
+        if request.form['inputPassword'] == password: # pw and password do not appear to be the same. Needs to be fixed.
                 login_user()
                 return render_template('index.html')
-	db.close()
+	else:
+		flash('incorrect password')
         return render_template('login.html')
 
 def login_user():
