490b25d865be23bf37bf363887afc1f44dff5342
baileywickham <baileywickham@gmail.com>
2017-07-10 21:03:30 -0500

    added session support

--- webtool.py
+++ webtool.py
@@ -1,10 +1,7 @@
-from flask import Flask, flash, render_template, request, redirect, url_for, g
+from flask import Flask, flash, render_template, request, redirect, url_for, make_response
 import flask_login
 from flask_login import current_user
 import MySQLdb
-import plivo
-from functools import wraps
-
 
 #TODO: login_user method, user_loader method
 # Prepare flask and login manager for use
@@ -19,44 +16,51 @@ login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
 users = {'admin': 'password'}
 app.secret_key = '1234' #TODO: THIS NEEDS TO BE CHANGED IN THE FUTURE
-
-auth_id = ''
-auth_token = ''
-
-#p = plivo.RestAPI(auth_id, auth_token) this can be commented out and left here
-
-
-
 db = MySQLdb.connect(host="localhost",
 		     user="root",
 		     passwd="alexiscool",
 		     db="panda-login")
 #CURSORS MUST BE INSIDE METHODS OR ELSE IT CRASHES, NO GLOBAL CURSORS. cur = db.cursor()
-@app.route('/')
-def splash():
-	if User.is_autenticated:
-		return redirect(url_for('home'))
-	else:
-		return redirect(url_for('login'))
-		
+
 def user_exists(username):
 	cur = db.cursor()
 	if cur.execute("SELECT * FROM Users WHERE username = %s", [username]) != 0:
+		cur.close()
 		return True
+	cur.close()
 	return False
 	# Non-zero value indicates that the user exists
 
 def get_password(username):
 	cur = db.cursor()
-	result = cur.execute("SELECT * FROM Users WHERE username = '" + username + "'")
+	result = cur.execute("SELECT * FROM Users WHERE username = '%s'", username)
 	if result > 0:
 		data = cur.fetchone()
-		dbpass = data[4]
-	return dbpass
+		user_pass = data[4]
+	cur.close()
+	return user_pass
 	# After gathering password, sha256 should be verified, see random paste above
 
+def get_id(username):
+	cur = db.cursor()
+	result = cur.execute("SELECT * FROM Users WHERE username = '%s'", username)
+	if result > 0:
+		data = cur.fetchone()
+		user_id = data[5] # This location could be wrong, I did it from memory
+	cur.close()
+	return user_id
+def cookie_exists(username):
+	if username in request.cookies: # request.cookies is a dict, as such we can use this if statement
+		return True
+	return False
+
+def set_cookie(username): # This was hashed together, really needs testing
+	response = make_response(render_template('login.html'))
+        response.set_cookie('username', username)
+        return response
+
 @app.route('/login')
-def loginfunc():
+def splash():
         return render_template('login.html')
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -68,42 +72,42 @@ def login():
 	if not user_exists(username):
 		flash('incorrect username')
 
-	password = get_password(username)
-	user_id = 1234
-       	User = UserClass(username, user_id, active=True)
-        if pw == password:
-                login_user()
-                return redirect(url_for('home')) 
+	dbpassword = get_password(username)
+	User = UserClass(username, user_id, active=True) # Do we actually need this? I think the same effect can be done with cookies
+        if pw == dbpassword:
+                flask_login.login_user(username) # Creates a login session
+		set_cookie(username)
+                home() 
 	else:
 		flash('incorrect password')
         return render_template('login.html')
 
-def login_user():
-        pass 
-#lol
+def logout(user):
+	flask_login.logout_user()
+	flash('Logged out successfully')
+
+@login_manager.unauthorized_handler
+def unauthorized():
+	return 'Unauthorized: you need to be logged in.'
 
 @app.route('/home')
+@flask_login.login_required
 def home():
 	return render_template('index.html')
-
-
-
-@app.route('/home', methods=['GET', 'POST'])
+"""
+@app.route('/home', methods=['GET', 'POST']
 def smscall():
-	if request.method == 'POST':
+	if request.method == 'POST'
 		when = form.request('inputDate')
 		where = form.request('inputLocation') #use where[:4] to grab the first 4 digits to call the store by number.
 		postition = form.requset('inputFob')
 		textingLocations = form.request('inputAskingLocations')
 		additonaltext = form.request('extraText')
-
-	#params = { 'src' : 'xxxxxxxxx', dst : '123456789<123456789', text : 'hello from bailey'} 
-	#response = p.send_message(params)
-
-
+"""		
 @login_manager.user_loader
 def user_loader(userid):
         pass
+# This is where the user object will be created using UserClass, this will likely be called from login_user# This is where the user's session gets initiated
 
 #this creates a user object, the usermixin is a premade flask user_login class.
 #this will probably need to be rewritten at somepoint
@@ -114,16 +118,6 @@ class UserClass(flask_login.UserMixin):
                 self.active = active
 def is_active(self):
         return self.active
-
-#this doesnt work, I dont even know what it is
-def login_required(g):
-	@wraps(f)
-	def decorated_function(*args, **kwargs):
-		if g.user is None:
-			return redirect(url_for('login', next=request.url))
-		return f(*args, **kwargs)
-	return decorated_function
-
 
 #remove debuger for production
 if __name__ == '__main__':
